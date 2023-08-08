fbbd12747db3028aef905f495b1baf47aef0b9b6
Saara Ghani <135561777+saaraMghani@users.noreply.github.com>
2023-08-08 15:57:36 +1200

    Update nzbirds.js

--- nzbirds.js
+++ nzbirds.js
@@ -1,4 +1,4 @@
-let URL = 'saaraMghani/nzbirds/nzbird.json';
+let URL = 'https://github.com/saaraMghani/nzbirds/blob/13793b54380716fc1766b36dd9139156fceeb556/nzbird.json';
 
 /* This function should load all of the birds into your bird-display */
 function loadAllBirds() {
